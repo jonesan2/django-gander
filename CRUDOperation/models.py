4b812cd5d8d4aa8f15549c64a01b74c654455e80
Andrew Jones <andrewrayjones@gmail.com>
2021-05-23 21:57:56 -0500

    override default primary key

--- CRUDOperation/models.py
+++ CRUDOperation/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class EmpModel(models.Model):
+    id=models.BigAutoField(primary_key=True)
     empname=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
     occupation=models.CharField(max_length=100)

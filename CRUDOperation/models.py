a46ccd860c1119c6db7c89e56309a63f50cdb46d
Andrew Jones <andrewrayjones@gmail.com>
2021-05-23 22:00:50 -0500

    fix typo

--- CRUDOperation/models.py
+++ CRUDOperation/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class EmpModel(models.Model):
     id=models.BigAutoField(primary_key=True)
-    tgo empname=models.CharField(max_length=100)
+    empname=models.CharField(max_length=100)
     email=models.CharField(max_length=100)
     occupation=models.CharField(max_length=100)
     salary=models.IntegerField()
